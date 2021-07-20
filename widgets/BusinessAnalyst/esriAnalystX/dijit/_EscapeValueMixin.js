a3814579976fc357d45324b1953cdc0d22f8df53
timchen@cathaylife.com.tw <timchen@cathaylife.com.tw>
2021-07-20 09:34:49 +0800

    add file
    add file

---
+++ widgets/BusinessAnalyst/esriAnalystX/dijit/_EscapeValueMixin.js
@@ -0,0 +1,4 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://@sbaseurl@/jsapi/jsapi/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+//>>built
+define(["dojo/_base/declare"],function(a){return a(null,{_escapeValue:function(a){return a.replace(/"/g,"\x26quot;")}})});
---
+++ widgets/BusinessAnalyst/esriAnalystX/dijit/_TemplatedMixin.js
@@ -0,0 +1,4 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://@sbaseurl@/jsapi/jsapi/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+//>>built
+define(["dojo/_base/declare","dijit/_TemplatedMixin","./_EscapeValueMixin"],function(a,b,c){return a([b,c],{})});
